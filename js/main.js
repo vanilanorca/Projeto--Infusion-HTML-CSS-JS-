2aec6910815accf46e73dff974e1fbe76c66f0cd
VINICIUS LOPES ALVES <61030197+vanilanorca@users.noreply.github.com>
2020-08-20 18:39:35 -0300

    main commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function(){
+
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js');
+    $body.classList.add('js');
+
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnable: 1024
+    })
+
+    var carouselImgs = new Carousel({
+        container: '.laptop-slider .slide-show',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+    var carouselQuotes = new Carousel({
+        container: '.quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+    
+})()
